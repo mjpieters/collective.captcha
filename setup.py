a101c9bac559d514eb01a3496325c1a187ea8129
Martijn Pieters <mj@zopatista.com>
2007-11-15 11:02:17 +0000

    Update long description with something sensible

--- setup.py
+++ setup.py
@@ -6,6 +6,21 @@ setup(name='collective.captcha',
       version=version,
       description="Stateless captcha generation and verification",
       long_description="""\
+Captchas without server state
+-----------------------------
+
+A view to generate a captcha image and/or wav file, and to verify user input
+against it.
+
+A cookie is used to transfer state from one request to the next. The state is
+used, together with a server-side secret, to create a random string of
+characters, which in turn is displayed as a captcha image, or transformed to
+an audio file.
+
+Requirements:
+  SkimpyGimpy_
+  
+_SkimpyGimpy: http://skimpygimpy.sourceforge.net/
 """,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
