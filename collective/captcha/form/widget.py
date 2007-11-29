acb0981d913b8906cb5bf56b114dab3888646b3d
Florian Schulze <florian.schulze@gmx.net>
2007-11-29 21:29:53 +0000

    Now the widget renders properly.

--- collective/captcha/form/widget.py
+++ collective/captcha/form/widget.py
@@ -1,5 +1,30 @@
-from zope.app.form.browser import TextWidget
+from zope.component import getMultiAdapter
+from zope.app.form.browser import ASCIIWidget
+from zope.app.form.browser.textwidgets import renderElement
+from zope.i18n import MessageFactory
+
+from Acquisition import aq_inner
 
 
-class CaptchaWidget(TextWidget):
-    pass
+_ = MessageFactory('collective.captcha')
+
+
+class CaptchaWidget(ASCIIWidget):
+    def __call__(self):
+        captcha = getMultiAdapter((aq_inner(self.context.context), self.request), name='captcha')
+        kwargs = {'type': self.type,
+                  'name': self.name,
+                  'id': self.name,
+                  'cssClass': self.cssClass,
+                  'style': self.style,
+                  'size': self.displayWidth,
+                  'extra': self.extra}
+        if self.displayMaxWidth:
+            kwargs['maxlength'] = self.displayMaxWidth # TODO This is untested.
+
+        return u"""<div class="captchaImage">%s</div>
+<div class="captchaAudio"><a href="%s" target="_blank">%s</a></div>
+%s""" % (captcha.image_tag(),
+         captcha.audio_url(),
+         _(u"Listen to audio for this captcha"),
+         renderElement(self.tag, **kwargs))
