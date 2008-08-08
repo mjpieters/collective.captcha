12a80091dd0f0ec82a2aa4623353112ff6623d7d
Stephan Holek <stefan@epy.co.at>
2008-08-08 21:07:06 +0000

    Clean up and turn the workaround into a proper fix.

--- collective/captcha/browser/captcha.py
+++ collective/captcha/browser/captcha.py
@@ -6,7 +6,6 @@ import sha
 import string
 import sys
 import time
-import logging
 
 from skimpyGimpy import skimpyAPI
 
@@ -32,26 +31,39 @@ VERAMONO = os.path.join(_package_home, 'arevmoit.bdf')
 
 _TEST_TIME = None
 
-logger = logging.getLogger('collective.captcha.browser.captcha')
-
 class Captcha(BrowserView):
     implements(ICaptchaView)
     
     _session_id = None
     __name__ = 'captcha'
     
+    def _setcookie(self, id):
+        """Set the session cookie"""
+        resp = self.request.response
+        if COOKIE_ID in resp.cookies:
+            # clear the cookie first, clearing out any expiration cookie
+            # that may have been set during verification
+            del resp.cookies[COOKIE_ID]
+        resp.setCookie(COOKIE_ID, id, path='/')
+
     def _generate_session(self):
-        """Ensure a session id exists"""
+        """Create a new session id"""
         if self._session_id is None:
             id = sha.new(str(random.randrange(sys.maxint))).hexdigest()
             self._session_id = id
-            
-            resp = self.request.response
-            if COOKIE_ID in resp.cookies:
-                # clear the cookie first, clearing out any expiration cookie
-                # that may have been set during verification
-                del resp.cookies[COOKIE_ID]
-            resp.setCookie(COOKIE_ID, id, path='/')
+            self._setcookie(id)
+
+    def _verify_session(self):
+        """Ensure session id and cookie exist"""
+        if not self.request.has_key(COOKIE_ID):
+            if self._session_id is None:
+                # This may happen e.g. when the user clicks the back button
+                self._generate_session()
+            else:
+                # This may happen e.g. when the user does not accept the cookie
+                self._setcookie(self._session_id)
+            # Put the cookie value into the request for immediate consumption
+            self.request.cookies[COOKIE_ID] = self._session_id
     
     def _generate_words(self):
         """Create words for the current session
@@ -110,31 +122,13 @@ class Captcha(BrowserView):
         
     def image(self):
         """Generate a captcha image"""
+        self._verify_session()
         self._setheaders('image/png')
-        self._check_session('image')
         return skimpyAPI.Png(self._generate_words()[0],
                              fontpath=VERAMONO).data()
     
     def audio(self):
         """Generate a captcha audio file"""
+        self._verify_session()
         self._setheaders('audio/wav')
-        self._check_session('audio')
         return skimpyAPI.Wave(self._generate_words()[0], WAVSOUNDS).data()
-
-    def _check_session(self, type):
-        """Make extra sure _generate_words finds a session cookie
-
-        Fixes http://plone.org/products/collective.captcha/issues/3
-        """
-        session = self.request.get(COOKIE_ID, '')
-        if session == '':
-            if self._session_id is None:
-                logger.warn('No cookie and no session id while rendering @@captcha/%s\n'
-                            'User agent: %s' % (type, self.request.get('HTTP_USER_AGENT', '')))
-                self._generate_session()
-            else:
-                logger.warn('No cookie but valid session id while rendering @@captcha/%s\n'
-                            'User agent: %s' % (type, self.request.get('HTTP_USER_AGENT', '')))
-                self.request.response.setCookie(COOKIE_ID, self._session_id, path='/')
-            self.request.set(COOKIE_ID, self._session_id)
-
